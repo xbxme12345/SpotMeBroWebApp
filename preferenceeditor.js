e272eccd8729351a60e8ab3102bb45e8e6a8b432
Kyle Kable <kablek@wit.edu>
2018-03-06 00:32:13 -0500

    preference editor complete

--- preferenceeditor.js
+++ preferenceeditor.js
@@ -14,12 +14,25 @@
 
 
   const btnSubmitPref = document.getElementById("submitPref");
+  const new_prefGen = document.getElementById("prefgender");
+  const new_style = document.getElementById("prefStyle");
 
   var fbRef = firebase.database().ref().child('Users');
 
 
   btnSubmitPref.addEventListener('click', e => {
-    
+
+    var userId = firebase.auth().currentUser.uid;
+
+
+    // creates the new subset of data, Preferences, for previous entry
+    fbRef.child(userId).child("Preferences").set({
+      // unlike mobile app, cannot have space between key's name
+      Preferred_Gender: new_prefGen.value,
+      Style: new_style.value
+    });
+    alert("preferences have been updated to \'" + new_prefGen.value + "\' and \'" + new_style.value + "\'");
+
   });
 
 
