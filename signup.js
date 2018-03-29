3b580b581f70d8ef380089c24387bef2e30856ad
Tonthatj <Tonthatj@wit.edu>
2018-03-29 15:53:46 -0400

    need help

--- signup.js
+++ signup.js
@@ -46,7 +46,7 @@
 
     var fbRef2 = firebase.database().ref();
 
-    fbRef2.child("Messages").once('value',function(snap)
+    fbRef2.child("Users").once('value',function(snap)
     {
       var retarr = snapshotToArray(snap)
       var index;
@@ -62,58 +62,63 @@
       if(tempname == "no name found")
       {
     // sign in
-    if( txtPassword.value == txtPassword2.value)
-    {
-      const promise = auth.createUserWithEmailAndPassword(email, pass);
-      promise.catch(e => alert(e.message));
-    }
-    else
-    {
-      console.log('passwords don\'t match');
-      alert("Passwords do not match");
-    }
-}
+        if( txtPassword.value == txtPassword2.value)
+        {
+          const promise = auth.createUserWithEmailAndPassword(email, pass);
+          promise.catch(e => alert(e.message));
 
-    else {
-      console.log('Name already used!!!!!!!');
-      alert("Passwords do not match");
-    }
-      });
+          // reference to save data must be set globally
+          var fbRef = firebase.database().ref().child('Users');
+          // add a real time listener
+          firebase.auth().onAuthStateChanged(firebaseUser => {
+            if (firebaseUser)
+            {
+                /* using .value only works here not at var initialization */
+                var uidPERM = firebaseUser.uid;
+
+                /* added Availability as an empty string because if you don't it will be
+                  read as null and causes problems later when the user inputs first avail time */
+                fbRef.child(uidPERM).set({
+                  Name: txtName.value,
+                  Answer: secA.value,
+                  Availability: "",
+                  Email: txtEmail.value,
+                  Gender: gender.value,
+                  Security: secQ.value,
+                });
+
+                // creates the new subset of data, Preferences, for previous entry
+                fbRef.child(uidPERM).child("Preferences").set({
+                  // unlike mobile app, cannot have space between key's name
+                  Preferred_Gender: prefgen.value,
+                  Style: liftStyle.value
+                });
+
+                // passes hashed id as variable in URL
+                window.location = "mainpage.html?uidPerm="+uidPERM;
+            }
+              else
+              {
+                console.log('not logged in');
+              }
+            })
 
 
-  // reference to save data must be set globally
-  var fbRef = firebase.database().ref().child('Users');
-  // add a real time listener
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
+        }
+        else
+        {
+          console.log('passwords don\'t match');
+          alert("Passwords do not match");
+        }
+      }
+      else
+      {
+        console.log('Name already used!!!!!!!');
+        alert("Passwords do not match");
+      }
+    });
 
-      /* using .value only works here not at var initialization */
-      var uidPERM = firebaseUser.uid;
 
-      /* added Availability as an empty string because if you don't it will be
-        read as null and causes problems later when the user inputs first avail time */
-      fbRef.child(uidPERM).set({
-        Name: txtName.value,
-        Answer: secA.value,
-        Availability: "",
-        Email: txtEmail.value,
-        Gender: gender.value,
-        Security: secQ.value,
-      });
-
-      // creates the new subset of data, Preferences, for previous entry
-      fbRef.child(uidPERM).child("Preferences").set({
-        // unlike mobile app, cannot have space between key's name
-        Preferred_Gender: prefgen.value,
-        Style: liftStyle.value
-      });
-
-      // passes hashed id as variable in URL
-      window.location = "mainpage.html?uidPerm="+uidPERM;
-    } else {
-      console.log('not logged in');
-    }
-  })
 
 
 
